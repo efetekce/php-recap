7d59136a093c4c9b8402e1df77de9cd1006ffa5c
Efe <efete_kce@proton.me>
2024-04-25 00:25:27 +0300

    project setup, printing variables

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>php recap</h1>
+    <?php 
+    $message = 'Hello';
+
+    echo $message . "double quote" . ' ' . 'adding strings together';
+    // we use the . to concatonate strings together
+
+    echo "$message Everyone."
+    // if we want to use variables within strings we must use double quotation mark.
+    ?>
+  </body>
+</html>
